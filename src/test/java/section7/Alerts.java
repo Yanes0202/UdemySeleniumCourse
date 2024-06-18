782e289b3143509febe857337ca86313f3ab2438
Adrian <60939933+Yanes0202@users.noreply.github.com>
2024-06-18 16:10:08 +0200

    18.06.2024 section 7 alerts

---
+++ src/test/java/section7/Alerts.java
@@ -0,0 +1,24 @@
+package section7;
+
+import adrian.com.managers.Driver;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class Alerts {
+
+    @Test
+    void alerts() {
+        WebDriver driver = Driver.getDriver();
+        driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+        driver.findElement(By.id("name")).sendKeys("Name");
+        driver.findElement(By.id("alertbtn")).click();
+        System.out.println(driver.switchTo().alert().getText());
+        driver.switchTo().alert().accept();
+
+        driver.findElement(By.id("name")).sendKeys("Name");
+        driver.findElement(By.id("confirmbtn")).click();
+        System.out.println(driver.switchTo().alert().getText());
+        driver.switchTo().alert().dismiss();
+    }
+}
